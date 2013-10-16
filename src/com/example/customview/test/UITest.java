e0ecf4b81b4ddb120a36bd960aafd01e74b1285c
kli <kevin.li@conquermobile.com>
2013-10-16 09:02:19 -0700

    Added more test procedures

--- src/com/example/customview/test/UITest.java
+++ src/com/example/customview/test/UITest.java
@@ -9,7 +9,8 @@ import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
 public class UITest extends UiAutomatorTestCase {
 	
-	public void testDemo() throws UiObjectNotFoundException {
+	// This test determines if the device in question is a Nexus 4
+	public void testOne() throws UiObjectNotFoundException {
 		getUiDevice().pressHome();
 		
 		UiObject settingsButton = new UiObject(new UiSelector().text("Settings"));
@@ -20,10 +21,50 @@ public class UITest extends UiAutomatorTestCase {
 		UiObject aboutButton = settingsView.getChildByText(new UiSelector().className(android.widget.TextView.class.getName()), "About phone");
 		aboutButton.clickAndWaitForNewWindow();
 		
-		// Loud Noises
 		UiScrollable aboutView = new UiScrollable(new UiSelector().scrollable(true));
 	    settingsView.setAsVerticalList();		
 	    UiObject modelNumber = aboutView.getChild(new UiSelector().text("Nexus 4"));
 	    assertTrue(modelNumber.getText().equals("Nexus 4"));
+	}
+	
+	// This test determines whether the bluetooth toggle is on in the settings menu
+	public void testTwo() throws UiObjectNotFoundException {
+		getUiDevice().pressHome();
+		
+		UiObject settingsButton = new UiObject(new UiSelector().text("Settings"));
+		settingsButton.clickAndWaitForNewWindow();
+		
+		UiScrollable settingsView = new UiScrollable(new UiSelector().scrollable(true));
+		settingsView.setAsVerticalList();
+		UiObject bluetoothButton = settingsView.getChildByText(new UiSelector().className(android.widget.TextView.class.getName()), "Bluetooth");
+		bluetoothButton.clickAndWaitForNewWindow();
+		
+	    UiObject toggleSwitch = new UiObject(new UiSelector().className("android.widget.Switch"));
+	    assertTrue(toggleSwitch.getText().equals("ON"));
+	}
+	
+	// This test should fail
+	public void testThree() throws UiObjectNotFoundException {
+		getUiDevice().pressHome();
+		
+		UiObject settingsButton = new UiObject(new UiSelector().text("Thistisntademocracy"));
+		settingsButton.clickAndWaitForNewWindow();
+		assertTrue(true);
+	}
+	
+	// This test determines whether the mobile data is on in the Data usage menu
+	public void testFour() throws UiObjectNotFoundException {
+		getUiDevice().pressHome();
+		
+		UiObject settingsButton = new UiObject(new UiSelector().text("Settings"));
+		settingsButton.clickAndWaitForNewWindow();
+		
+		UiScrollable settingsView = new UiScrollable(new UiSelector().scrollable(true));
+		settingsView.setAsVerticalList();
+		UiObject dataButton = settingsView.getChildByText(new UiSelector().className(android.widget.TextView.class.getName()), "Data usage");
+		dataButton.clickAndWaitForNewWindow();
+		
+		UiObject dataToggle = new UiObject(new UiSelector().className("android.widget.Switch"));
+		assertTrue(dataToggle.getText().equals("ON"));
 	}
 }
