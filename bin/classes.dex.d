9dcbddca063844e610a19e8084c6703e1025768c
kli <kevin.li@conquermobile.com>
2013-10-16 09:18:09 -0700

    More commits

--- bin/classes.dex.d
+++ bin/classes.dex.d
@@ -1,7 +1,7 @@
-/Users/kli/Documents/mio_workspace/UITest/bin/classes.dex : \
-/Users/kli/Documents/mio_workspace/UITest/bin/classes/com/example/customview/test/BuildConfig.class \
-/Users/kli/Documents/mio_workspace/UITest/bin/classes/com/example/customview/test/R$attr.class \
-/Users/kli/Documents/mio_workspace/UITest/bin/classes/com/example/customview/test/R$drawable.class \
-/Users/kli/Documents/mio_workspace/UITest/bin/classes/com/example/customview/test/R$string.class \
-/Users/kli/Documents/mio_workspace/UITest/bin/classes/com/example/customview/test/R.class \
-/Users/kli/Documents/mio_workspace/UITest/bin/classes/com/example/customview/test/UITest.class \
+/Users/kli/Documents/UITest_/bin/classes.dex : \
+/Users/kli/Documents/UITest_/bin/classes/com/example/customview/test/BuildConfig.class \
+/Users/kli/Documents/UITest_/bin/classes/com/example/customview/test/R$attr.class \
+/Users/kli/Documents/UITest_/bin/classes/com/example/customview/test/R$drawable.class \
+/Users/kli/Documents/UITest_/bin/classes/com/example/customview/test/R$string.class \
+/Users/kli/Documents/UITest_/bin/classes/com/example/customview/test/R.class \
+/Users/kli/Documents/UITest_/bin/classes/com/example/customview/test/UITest.class \
