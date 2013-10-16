0334264146953c9def9fbc4bdc425e594ba1110f
kli <kevin.li@conquermobile.com>
2013-10-16 13:40:11 -0700

    Changed to Sound

--- src/com/example/customview/test/UITest.java
+++ src/com/example/customview/test/UITest.java
@@ -18,7 +18,7 @@ public class UITest extends UiAutomatorTestCase {
 		
 		UiScrollable settingsView = new UiScrollable(new UiSelector().scrollable(true));
 		settingsView.setAsVerticalList();
-		UiObject aboutButton = settingsView.getChildByText(new UiSelector().className(android.widget.TextView.class.getName()), "About phone");
+		UiObject aboutButton = settingsView.getChildByText(new UiSelector().className(android.widget.TextView.class.getName()), "Themes");
 		aboutButton.clickAndWaitForNewWindow();
 	}
 }
